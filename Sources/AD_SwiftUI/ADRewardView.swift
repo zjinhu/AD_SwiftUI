84f39233c519da1da949ab1cba2bcdd36d2b2841
虎 <814030966@qq.com>
2023-09-26 18:50:19 +0800

    new

--- Sources/AD_SwiftUI/ADRewardView.swift
+++ Sources/AD_SwiftUI/ADRewardView.swift
@@ -25,64 +25,49 @@ public struct ADRewardModifier: ViewModifier,RewardADInstanceProtocol {
     public func getRewardedCount() {
         action()
     }
-
-    private var adInstanse: ADRewardInstance?
+    
+    private let adInstanse = ADRewardInstance.shared
     private let action: () -> ()
     public init(adUnitID: String, action: @escaping () -> ()) {
         self.action = action
-        adInstanse = ADRewardInstance(adUnitID: adUnitID)
-        adInstanse?.delegate = self
-        
+        adInstanse.adInstance.setAdUnitID(adUnitID)
+        adInstanse.delegate = self
     }
 
     public func body(content: Content) -> some View {
         content
             .onAppear{
-                adInstanse?.loadAd()
+                adInstanse.loadAd()
             }
             .onTapGesture {
-                adInstanse?.showAD()
+                adInstanse.showAD()
             }
     }
 }
 
 public class ADRewardInstance: NSObject, TradPlusADRewardedDelegate {
-
     public var delegate: RewardADInstanceProtocol?
-    
-    var adInstance: TradPlusAdRewarded?
-    
-    private let adUnitID: String
-    
-    public init(adUnitID: String) {
-        self.adUnitID = adUnitID
-    }
- 
-    private func clean() {
-        adInstance = nil
-    }
-    
+    public lazy var adInstance: TradPlusAdRewarded = {
+        let adInstance = TradPlusAdRewarded()
+        adInstance.delegate = self
+        return adInstance
+    }()
+    public static let shared = ADRewardInstance()
+    private override init() {}
+
     public func loadAd() {
-        clean()
-        adInstance = TradPlusAdRewarded()
-        adInstance?.setAdUnitID(adUnitID)
-        adInstance?.delegate = self
-        adInstance?.loadAd()
+        adInstance.loadAd()
     }
     
     public func loadAd() async throws {
-        clean()
         return try await withCheckedThrowingContinuation { continuation in
-            adInstance = TradPlusAdRewarded()
-            adInstance?.setAdUnitID(adUnitID)
-            adInstance?.delegate = self
-            adInstance?.loadAd()
+            adInstance.loadAd()
         }
     }
     
     public func showAD(){
-        if adInstance?.isAdReady == true{
-            adInstance?.showAd(withSceneId: nil)
+        if adInstance.isAdReady == true{
+            adInstance.showAd(withSceneId: nil)
         }
     }
     
@@ -108,7 +93,6 @@ public class ADRewardInstance: NSObject, TradPlusADRewardedDelegate {
     
     public func tpRewardedAdDismissed(_ adInfo: [AnyHashable : Any]) {
         debugPrint("Rewarded AdDismissed")
-        clean()
     }
     
     public func tpRewardedAdReward(_ adInfo: [AnyHashable : Any]) {
